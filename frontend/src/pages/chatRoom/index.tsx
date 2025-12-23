e9d54014dbedb73adeca7418e7621ae9e4a5f558
임승현 <96limshyun@gmail.com>
2025-12-23 22:41:28 +0900

    feat: update chat room page

--- frontend/src/pages/chatRoom/index.tsx
+++ frontend/src/pages/chatRoom/index.tsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom"
+
 const ChatRoom = () => {
+  const { id } = useParams()
+  console.log(id)
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Chat Room</h1>
