00bcb927468d0b37870e4314f7cc315f029f1cb0
임승현 <96limshyun@gmail.com>
2025-12-23 18:14:21 +0900

    feat: Add home and chat room page components

---
+++ frontend/src/pages/chatRoom/index.tsx
@@ -0,0 +1,9 @@
+const ChatRoom = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Chat Room</h1>
+    </div>
+  )
+}
+
+export default ChatRoom
