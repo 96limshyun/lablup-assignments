325ba24dcb9e184de613d8d6a1ed0dbc71768e7b
임승현 <96limshyun@gmail.com>
2025-12-23 18:13:58 +0900

    feat: Setup React Router with route configuration

---
+++ frontend/src/routers/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "@/pages/home/index";
+import ChatRoom from "@/pages/chatRoom/index";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/room/:id", element: <ChatRoom />},
+]);
+
+export default routes;
