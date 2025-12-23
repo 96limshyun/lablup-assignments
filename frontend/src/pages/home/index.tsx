6b6d8bf61578e858da37fe3c6ae3f1e99d8bcf5c
임승현 <96limshyun@gmail.com>
2025-12-23 19:06:54 +0900

    refactor: Refactor home page to use custom hook and service layer

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,7 +1,17 @@
+import useRoomManagement from "./hooks/useRoomManagement"
+
 const Home = () => {
+  const { rooms, handleCreateRoom, handleJoinRoom } = useRoomManagement()
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Home</h1>
+      <button onClick={() => handleCreateRoom('test123')}>Create Room</button>
+      <div className="flex flex-col items-center justify-center">
+        {rooms.map((room) => (
+          <div key={room.id} onClick={() => handleJoinRoom(room.id)}>{room.name}</div>
+        ))}
+      </div>
     </div>
   )
 }
