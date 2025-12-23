3ff5e0c69532d22d6cf7eba2bf3812bbe9689de0
임승현 <96limshyun@gmail.com>
2025-12-23 19:06:35 +0900

    feat: Add useRoomManagement custom hook

---
+++ frontend/src/pages/home/hooks/useRoomManagement.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import apiFetch from "@/services/fetch"
+
+type Room = {
+  id: string
+  name: string
+  created_at: string
+}
+
+const useRoomManagement = () => {
+  const navigate = useNavigate()
+  const [rooms, setRooms] = useState<Room[]>([])
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const rooms = await apiFetch('/rooms', 'GET')
+      setRooms(rooms)
+    }
+    fetchRooms()
+  }, [])
+
+  const handleCreateRoom = async (userId: string) => {
+    const room = await apiFetch('/rooms', 'POST', { name: userId })
+    navigate(`/room/${room.id}`)
+  }
+  
+
+  const handleJoinRoom = async (roomId: string) => {
+    navigate(`/room/${roomId}`)
+  }
+  return {
+      rooms,
+      handleCreateRoom,
+      handleJoinRoom
+    }
+}
+
+export default useRoomManagement
